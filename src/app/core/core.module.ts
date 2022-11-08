3c7c9b9aff9c906746fb7013a09b490c88e68b9e
Uros Maksimovic <maksimovic.u@yahoo.com>
2022-11-08 12:05:50 +0100

    small fix

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
-import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
 
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
